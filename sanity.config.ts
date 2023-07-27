acf3803050679f775369701791b05182ab2e87f6
Jubair Ahmed <jubairahmed3000@gmail.com>
2023-07-27 17:30:17 +0530

    Making Changes in sanity config.jd

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,47 +1,61 @@
+// import { defineConfig } from 'sanity';
+
+// import { deskTool } from 'sanity/desk';
+
+// import schemas from "./sanity/schemas"
+
+// import sanityClient from '@sanity/client'
+
+
+
+
+// const config = defineConfig({
+
+//   projectId: '8sfwwolx',
+
+//   dataset: 'production',
+ 
+//   title: "My Portfolio Website ",
+
+//   apiVersion: "2023-07-26",
+
+//   basePath: "/admin",
+
+//   plugins: [deskTool()],
+
+//   schema: { types: schemas },
+
+ 
+// })
+
+// export default config
+
+
+// export const client = sanityClient({
+//   projectId: '8sfwwolx',
+  
+//   dataset: 'production',
+  
+//   title: "My Portfolio Website ",
+  
+//   apiVersion: "2023-07-26",
+  
+   
+// }) 
+
 import { defineConfig } from 'sanity';
-
 import { deskTool } from 'sanity/desk';
-
 import schemas from "./sanity/schemas"
-
-import sanityClient from '@sanity/client'
-
-
 
 
 const config = defineConfig({
-
   projectId: '8sfwwolx',
-
-  dataset: 'production',
- 
-  title: "My Portfolio Website ",
-
+  dataset: "production",
+  title: "My Personal Website",
   apiVersion: "2023-07-26",
-
   basePath: "/admin",
-
   plugins: [deskTool()],
-
-  schema: { types: schemas },
-
-  useCdn: true
+  schema: { types: schemas }
 })
 
-
-
-
 export default config
-
-
-export const client = sanityClient({
-  projectId: '8sfwwolx',
-  
-  dataset: 'production',
-  
-  title: "My Portfolio Website ",
-  
-  apiVersion: "2023-07-26",
-  
-  useCdn: true
-}) 
