3ac38a18bf4114534cf8832dfee4d9c9e9f60566
Jubair Ahmed <jubairahmed3000@gmail.com>
2023-07-28 21:08:09 +0530

    Adding Some Styles

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
     <html lang="en">
 
 
-      <body className="md:px-[5rem] bg-primary-black md:m-[4rem]  m-[0.6rem] px-[1rem]">
+      <body className="md:px-[5rem] bg-primary-black md:m-[4rem]  m-[0.6rem] px-[0.69rem]">
         <header className="flex items-center justify-between">
           
           <Navbar pages={pages} />
